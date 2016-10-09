afacddf169f237507198261b6a772d4978a68d61
Kargin Nikita <hei@spark-mail.ru>
2016-10-09 15:42:46 +0300

    // TODO

--- src/main/java/servlets/NewGameServlet.java
+++ src/main/java/servlets/NewGameServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
+import beans.UserEntity;
 import entityservices.QueryConfiguration;
+import entityservices.UserEntityService;
 import org.hibernate.Session;
 
 import javax.servlet.ServletException;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class NewGameServlet extends HttpServlet {
 
@@ -21,6 +24,19 @@ public class NewGameServlet extends HttpServlet {
         Session session =
                 QueryConfiguration.getInstance().getFactory().openSession();
 
+        Map<String, String[]> paramsMap = req.getParameterMap();
+        UserEntityService firstUser = new UserEntityService(session);
+        firstUser.getUserByEmail(paramsMap.get("e-mail")[0]).setIs_ready(true);
+
+        /*
+        THREAD!
+        !! AT THIS MOMENT WILL BE CREATED A RUNNABLE COMPONENT !!
+        Watching for users who had is_ready - true option.
+        Gets this User.
+        Creating the Session of User1 & User2 game.
+        Send to users
+        When the game has already started, we're handling the Post requests.
+         */
 
     }
 
