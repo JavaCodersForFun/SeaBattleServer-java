9a2949a41e8cf07afb76da7f1eff887c3b6f707f
Kargin Nikita <hei@spark-mail.ru>
2016-10-09 17:51:34 +0300

    Deleted doGet.

--- src/main/java/servlets/NewGameServlet.java
+++ src/main/java/servlets/NewGameServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import beans.UserEntity;
 import entityservices.QueryConfiguration;
 import entityservices.UserEntityService;
 import org.hibernate.Session;
@@ -28,23 +29,7 @@ public class NewGameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        Session session =
-                QueryConfiguration.getInstance().getFactory().openSession();
 
-        Map<String, String[]> paramsMap = req.getParameterMap();
-        UserEntityService firstUser = new UserEntityService(session);
-        firstUser.getUserByEmail(paramsMap.get("e-mail")[0]).setIs_ready(true);
-
-        /*
-        TODO!
-        THREAD!
-        !! AT THIS MOMENT WILL BE CREATED A RUNNABLE COMPONENT !!
-        Watching for users who had is_ready - true option.
-        Gets this User.
-        Creating the Session of User1 & User2 game.
-        Send to users
-        When the game has already started, we're handling the Post requests.
-         */
 
     }
 
