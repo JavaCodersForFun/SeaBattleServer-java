ebbee07915cfcffd51c6791ad8cd20d38ba88066
Kargin Nikita <hei@spark-mail.ru>
2016-10-13 22:47:34 +0300

    Created GetPositionServlet

---
+++ src/main/java/servlets/GetPositionServlet.java
@@ -0,0 +1,20 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GetPositionServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Gets position from User
+        // Write position to buffered field
+        // Returns status.
+
+    }
+
+}
