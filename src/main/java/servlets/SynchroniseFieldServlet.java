bb4b5bd447bbefc84fc302521d74f764eaee5633
Kargin Nikita <hei@spark-mail.ru>
2016-10-13 10:57:05 +0300

    Created SynchoriseFieldServlet

---
+++ src/main/java/servlets/SynchroniseFieldServlet.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SynchroniseFieldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Getting up the session_id and user from request
+        // Check the equality with db
+        //
+
+    }
+}
